7f73ed357b688b64b9e7ff45576a46eb437d1306
B.Vamsi Krishna <bommisettyvamsikrishna04@gmail.com>
2018-10-02 00:42:34 +0530

    Create Power(x,n).py
    Added a Python file that Implements pow(x, n), which calculates x raised to the power n (x^n).

---
+++ Math/Power(x,n).py
@@ -0,0 +1,7 @@
+# Implemented pow(x, n), which calculates x raised to the power n (x^n).
+# This will import math module
+import math
+
+# Input: 2.00000, 10
+print "math.pow(2.00000, 10) : ", math.pow(2.00000, 10)
+# Output: 1024.00000
